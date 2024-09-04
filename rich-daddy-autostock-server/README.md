102c54af3934552af67c00f3cea6139e7e81b68c
Hum <60500720+huml0g@users.noreply.github.com>
2024-09-04 17:32:10 +0900

    Delete rich-daddy-autostock-server directory

--- rich-daddy-autostock-server/README.md
+++
@@ -1,7 +0,0 @@
-## Build Setup
-$ npm install
-
-## Server Start
-$ npm start"# rich-daddy-autostock-server" 
-"# rich-daddy-autostock-server" 
-"# rich-daddy-autostock-server" 
--- rich-daddy-autostock-server/src/core/async-error-handler.js
+++
@@ -1,9 +0,0 @@
-module.exports = (asyncFunction) => (
-    async (req, res, next) => {
-        try {
-            await asyncFunction(req, res, next);
-        } catch (error) {
-            next(error);
-        }
-    }
-)
